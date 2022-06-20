8b6c04c28b3b89f29a1a1208ad2dd8bc71417c22
sig03 <sig03@naver.com>
2022-06-20 08:13:10 +0000

    add globalStyle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,40 @@ import type { AppProps } from 'next/app';
 import { Layout } from '/src/components/';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { createGlobalStyle } from 'styled-components';
 
 const queryClient = new QueryClient();
 
+const GlobalStyles = createGlobalStyle`
+  html,
+  body {
+      padding: 0;
+      margin: 0;
+      font-family: 'Noto Sans KR', 'Arial', sans-serif;
+      line-height: 1.5;
+  }
+
+  a {
+      color: inherit;
+      text-decoration: none;
+  }
+
+  *, *::before, *::after {
+      box-sizing: border-box;
+  }
+`;
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </Layout>
+    <>
+      <GlobalStyles />
+      <Layout>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </Layout>
+    </>
   );
 }
 
