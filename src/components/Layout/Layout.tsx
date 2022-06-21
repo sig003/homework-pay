1d956ac77c11629f313d0cb2c64dedbf8544ab13
sig03 <sig03@naver.com>
2022-06-21 06:17:36 +0000

    modify main contents

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -15,15 +15,30 @@ const HeaderLayout = styled.div`
   height: 50px;
 `;
 
+const AsideSectionLayout = styled.div`
+  display: flex;
+`;
+
+const AsideLayout = styled.div`
+  width: 15%;
+  height: calc(100vh - 50px);
+`;
+
+const SectionLayout = styled.div`
+  width: 85%;
+  height: calc(100vh - 50px);
+  background-color: hsl(0, 6%, 93%);
+`;
+
 export default function Layout({ children }: ChildrenProps) {
   return (
     <>
       <LayoutWrapper>
         <HeaderLayout>Top</HeaderLayout>
-        <div>
-          <div>aside</div>
-          <div>{children}</div>
-        </div>
+        <AsideSectionLayout>
+          <AsideLayout>aside</AsideLayout>
+          <SectionLayout>{children}</SectionLayout>
+        </AsideSectionLayout>
       </LayoutWrapper>
     </>
   );
--- src/pages/charge/Charge.tsx
+++ src/pages/charge/Charge.tsx
@@ -1,7 +1,31 @@
-export default function Charge() {
+import styled from 'styled-components';
+
+const ContentsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+`;
+
+const ContentBox = styled.div`
+  width: 60rem;
+  height: 10rem;
+  border-radius: 10px;
+  background-color: white;
+`;
+
+function Charge() {
   return (
     <>
-      <div>fefe</div>
+      <ContentsWrapper>
+        <ContentBox>section 1</ContentBox>
+        <ContentBox>section 2</ContentBox>
+        <ContentBox>section 3</ContentBox>
+      </ContentsWrapper>
     </>
   );
 }
+
+export default Charge;
