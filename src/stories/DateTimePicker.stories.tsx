af7d35bde5a8babb5439ef8a56612f5365a30ef8
sig03 <sig03@naver.com>
2022-06-25 02:23:57 +0000

    modify DateTimePicker story

---
+++ src/stories/DateTimePicker.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { DateTimePicker } from '/src/components';
+
+export default {
+  title: 'Components/DateTimePicker',
+  component: DateTimePicker,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    DateTimePicker => (
+      <div style={{ padding: '3rem' }}>
+        <DateTimePicker />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof DateTimePicker>;
+
+const Template: ComponentStory<typeof DateTimePicker> = args => (
+  <DateTimePicker {...args} />
+);
+
+export const Basic = Template.bind({});
+Basic.args = { label: 'Start' };
+//export const Basic = () => <DateTimePicker />;
