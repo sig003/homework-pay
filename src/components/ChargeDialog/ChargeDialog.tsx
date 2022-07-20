581ac7d1714383d3e454c0be76a4ec3b13af640c
sig03 <sig03@naver.com>
2022-07-20 02:46:40 +0000

    add receipt dialog

--- src/components/ChargeDialog/ChargeDialog.tsx
+++ src/components/ChargeDialog/ChargeDialog.tsx
@@ -1,31 +1,91 @@
-import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemText from '@mui/material/ListItemText';
-import DialogTitle from '@mui/material/DialogTitle';
+
 import Dialog from '@mui/material/Dialog';
-import PersonIcon from '@mui/icons-material/Person';
-import AddIcon from '@mui/icons-material/Add';
-import Typography from '@mui/material/Typography';
-import { blue } from '@mui/material/colors';
+import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 interface ChargeDialogProps {
-	open: boolean;
-	onClose: (value: string) => void;
+  open: boolean;
+  onClose: () => void;
 }
 
-export default function ChargeDialog(props: SimpleDialogProps) {
-	const { onClose, open } = props;
+interface PriceProps {
+  [index: string]: number;
+}
 
-	const handleClose = () => {
-		onClose();
-	};
-	return (
-		<Dialog onClose={handleClose} open={open}>
-			<DialogTitle>Set backup account</DialogTitle>
-			aaas
-    </Dialog>
-	);
+export default function ChargeDialog(props: ChargeDialogProps) {
+  const { onClose, open } = props;
+  const item = useSelector(state => state.item);
+  const [totalSum, setTotalSum] = useState(0);
+
+  const price: PriceProps = {
+    phone: 30,
+    pc: 50,
+    light: 90,
+    print: 10,
+    coffee: 100,
+    dining: 200,
+    desert: 150,
+    drink: 120,
+  };
+
+  useEffect(() => {
+    item.map((list: string) => {
+      setTotalSum(totalSum + price[list]);
+    });
+  }, [item]);
+
+  const Dash = styled.div`
+    border-bottom: 1px dashed hsl(0, 0%, 0%);
+    width: 92%;
+    margin: 1rem 1rem 1rem 1rem;
+  `;
+
+  const ReceiptWrapper = styled.div`
+    diplay: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 400px;
+  `;
+
+  const Item = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 1rem;
+  `;
+
+  const Total = styled.div`
+    font-weight: 600;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 1rem;
+  `;
+
+  const handleClose = () => {
+    onClose();
+  };
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <ReceiptWrapper>
+        <h2>RECEIPT</h2>
+        <Dash />
+        {item.map((list: string) => {
+          return (
+            <Item key={list}>
+              <span>{list}</span>
+              <span>$ {price[list]}</span>
+            </Item>
+           );
+         })}
+       <Dash />
+       <Total>
+         <span>Total</span>
+         <span>$ {totalSum}</span>
+        </Total>
+     </ReceiptWrapper>
+  </Dialog>
+  );
 }
