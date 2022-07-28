712f3a16cadaf0617a3309bd8c6ae80214a5bae8
sig03 <sig03@naver.com>
2022-07-28 07:53:02 +0000

    modify typescript

--- src/components/DateTimePicker/DateTimePicker.tsx
+++ src/components/DateTimePicker/DateTimePicker.tsx
@@ -7,7 +7,7 @@ import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 interface LabelProps {
   label: string;
 }
-
+//https://next.material-ui-pickers.dev/guides/typescript
 export default function BasicDateTimePicker({ label }: LabelProps) {
   const [value, setValue] = useState<Date | null>(new Date());
 
@@ -16,10 +16,10 @@ export default function BasicDateTimePicker({ label }: LabelProps) {
       <DateTimePicker
         inputFormat="yyyy-MM-dd hh:mm a"
         mask="___-__-__ __:__ _M"
-        renderInput={(props: string) => <TextField {...props} />}
+        renderInput={props => <TextField {...props} />}
         label={label}
         value={value}
-        onChange={(newValue: Date) => {
+        onChange={newValue => {
           setValue(newValue);
         }}
       />
