095f6cbf17562cd83b01b08fb8454d54f5563fab
sig03 <sig03@naver.com>
2022-08-04 00:01:26 +0000

    add testing file

---
+++ __tests__/Charge.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Charge from 'src/pages/charge/Charge';
+
+describe('<Charge />', () => {
+    /*it('render 404 Not found page', () => {
+      render(<NotFound />);
+  
+      screen.getByText('404. Page not found.');
+    });*/
+});
