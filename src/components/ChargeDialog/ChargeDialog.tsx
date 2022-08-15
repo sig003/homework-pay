3594033fe3a1947f1f2a223653eb5fd113f58f60
sig03 <sig03@naver.com>
2022-08-15 07:36:25 +0000

    modify charge dialog size

--- src/components/ChargeDialog/ChargeDialog.tsx
+++ src/components/ChargeDialog/ChargeDialog.tsx
@@ -18,49 +18,55 @@ interface PriceProps {
 }
 
 const Dash = styled.div`
-border-bottom: 1px dashed hsl(0, 0%, 0%);
-width: 92%;
-margin: 1rem 1rem 1rem 1rem;
+  border-bottom: 1px dashed hsl(0, 0%, 0%);
+  width: 92%;
+  margin: 1rem 1rem 1rem 1rem;
 `;
 
 const ReceiptWrapper = styled.div`
-diplay: flex;
-justify-content: center;
-align-items: center;
-text-align: center;
-width: 400px;
+  diplay: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 400px;
+  @media screen and (max-width: 600px) {
+    width: 300px;
+  }
 `;
 
 const Item = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-padding: 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 1rem;
 `;
 
 const Total = styled.div`
-font-weight: 600;
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-padding: 1rem;
+  font-weight: 600;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 1rem;
 `;
 
 const ChargedStamp = styled.div`
-border: 4px dashed red;
-border-radius: 90%;
-width: 100px;
-height: 100px;
-color: red;
-font-weight: 600;
-display: flex;
-justify-content: center;
-align-items: center;
-z-index: 100;
-position: absolute;
-top: 40%;
-left: 40%;
-animation-delay: 1s;
+  border: 4px dashed red;
+  border-radius: 90%;
+  width: 100px;
+  height: 100px;
+  color: red;
+  font-weight: 600;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+  position: absolute;
+  top: 40%;
+  left: 40%;
+  animation-delay: 1s;
+  @media screen and (max-width: 600px) {
+    left: 35%;
+  }
 `;
 
 export default function ChargeDialog(props: ChargeDialogProps) {
