f9cdd0945272e729ae8a0267aed66d664d4d528a
sig03 <sig03@naver.com>
2022-08-21 05:51:51 +0000

    modify 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,18 @@
+import styled from 'styled-components';
+
 export default function PageNotFound() {
-  return <div>404. Page not found.</div>;
+  const Wrapper = styled.div`
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    height: 40%;
+    font-weight: 600;
+    font-size: 24px;
+  `;
+
+  return (
+    <>
+      <Wrapper>404. Page not found.</Wrapper>
+    </>
+  );
 }
