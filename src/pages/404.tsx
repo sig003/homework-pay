520ab102fdc109bf064bad38830ca5c31ceffe02
sig03 <sig03@naver.com>
2022-08-22 00:00:51 +0000

    modify styled-components position

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
-export default function PageNotFound() {
-  const Wrapper = styled.div`
-    display: flex;
-    align-items: flex-end;
-    justify-content: center;
-    height: 40%;
-    font-weight: 600;
-    font-size: 24px;
-  `;
+const Wrapper = styled.div`
+display: flex;
+align-items: flex-end;
+justify-content: center;
+height: 40%;
+font-weight: 600;
+font-size: 24px;
+`;
 
+export default function PageNotFound() {
   return (
     <>
       <Wrapper>404. Page not found.</Wrapper>
